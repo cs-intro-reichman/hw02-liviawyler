5b2905db7900a4e87f5b4d3a4c006d81b539edce
liviawyler <livia.wyler@runi.ac.il>
2024-01-02 13:17:13 +0200

    Damnka

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,15 +5,18 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		//// Put your code here
 		int num = Integer.parseInt(args[0]);
-		for (int row = 1; row <= num; row ++) {
+		for (int row = 0; row < num; row ++) {
 			if (row % 2 == 0) {
-				System.out.print(" ");
+				for (int col = 0; col < num; col ++) {
+					System.out.print(" *");
+				}
 			}	
-			for (int col = 1; col <= num; col ++) {
-				System.out.print("* ");
-
-
+			else {
+				for (int col = 0; col < num; col ++) {
+					System.out.print("* ");
+				}
 			}
+			
 			System.out.println("");
 		}
 	}
